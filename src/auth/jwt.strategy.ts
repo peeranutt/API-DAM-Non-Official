69a71f6c7f1cdea483151a65c81ae5879f52d024
peeranutt <60286536+peeranutt@users.noreply.github.com>
2025-12-26 14:35:17 +0700

    get user login

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -27,7 +27,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any) {
     console.log('JWT PAYLOAD =', payload);
     return {
-      id: payload.id,
+      id: payload.sub,
       username: payload.username,
       role: payload.role,
     };
